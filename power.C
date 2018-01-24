c62ba46a70880bf2ac8eb8829b23f638ce3f9730
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-01-24 16:09:22 +0530

    power.C
    power of anumber

---
+++ power.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int s,p,power=1;
+printf("enter the value of s:");
+scanf("%d",&s);
+printf("enter the power of a no p:");
+scanf("%d",&p);
+while(p!=0)
+{
+power=p*power;
+--p;
+}
+printf("power is %d",power);
+return 0;
+}
